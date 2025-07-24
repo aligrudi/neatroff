aligrudi/neatroff

commit a3377daa639dc73982a432af4f79b0fe90852396

font: read font section in gsub command

font.c
@@ -206,7 +206,7 @@ static void font_performgpos(struct font *fn, int *src, int slen,
 						src + i, i, &idx);
 			if (r < 0)		/* no rule found */
 				break;
-			if (gpos[r].sec > 0 && gpos[r].sec == lastsec)
+			if (gpos[r].sec > 0 && gpos[r].sec <= lastsec)
 				continue;	/* perform at most one rule from each lookup */
 			lastsec = gpos[r].sec;
 			pats = gpos[r].pats;
@@ -609,6 +609,7 @@ struct font *font_open(char *path)
 	FILE *fin;
 	char ligs[512][GNLEN];
 	int ligs_n = 0;
+	int sec;
 	int i;
 	fin = fopen(path, "r");
 	if (!fin)
@@ -636,7 +637,8 @@ struct font *font_open(char *path)
 					ligs_n++;
 			}
 		} else if (!strcmp("gsec", tok)) {
-			fn->secs++;
+			if (fscanf(fin, "%d", &sec) != 1)
+				fn->secs++;
 		} else if (!strcmp("gsub", tok)) {
 			font_readgsub(fn, fin);
 		} else if (!strcmp("gpos", tok)) {

commit 6fe7d09dae925a35e80d2fe6c665339d029304a8

font: fix broken processing of positioning rules
Multiple rules from a single lookup table (GSEC) were being applied
due to the counting of GSECs being broken, resulting in incorrect
positioning when more than one rule within a lookup table applied
to a given combination of glyphs.

Fixes: a3377daa639dc73982a432af4f79b0fe90852396
Introduced-In: 545147c3896747fce52f922fe984f40ecc10bea3

font.c
@@ -632,7 +632,7 @@ struct font *font_open(char *path)
 					ligs_n++;
 			}
 		} else if (!strcmp("gsec", tok)) {
-			if (fscanf(fin, "%d", &sec) != 1)
+			if (fscanf(fin, "%d", &sec) == 1)
 				fn->secs++;
 		} else if (!strcmp("gsub", tok)) {
 			font_readgsub(fn, fin);