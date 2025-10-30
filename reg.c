aligrudi/neatroff

commit e57ccd9904b6a9b40087d8c57e9972c9f7a822b8

reg: evc request
.evc ENV
  Copy text-formatting properties of environment ENV to currently
  active environment.

reg.c
@@ -27,7 +27,7 @@ static int nregs_inc[NREGS];	/* number register auto-increment size */
 static int nregs_fmt[NREGS];	/* number register format */
 static char *sregs[NREGS];	/* global string registers */
 static void *sregs_dat[NREGS];	/* builtin function data */
-static struct env *envs[NREGS];/* environments */
+static struct env *envs[NREGS];	/* environments */
 static struct env *env;		/* current enviroment */
 static int env_id;		/* current environment id */
 static int eregs_idx[NREGS];	/* register environment index in eregs[] */
@@ -312,6 +312,24 @@ void tr_ev(char **args)
 	if (args[1] && env && nenv < NPREV)
 		oenv[nenv++] = env_id;
 	env_set(id);
+}
+
+void tr_evc(char **args)
+{
+	struct env *src;
+	int id = -1;
+	if (args[1])
+		id = map(args[1]);
+	if (id < 0 || !envs[id] || id == env_id)
+		return;
+	src = envs[id];
+	memcpy(env->eregs, src->eregs, sizeof(env->eregs));
+	memcpy(env->tabs, src->tabs, sizeof(env->tabs));
+	memcpy(env->tabs_type, src->tabs_type, sizeof(env->tabs_type));
+	memcpy(env->tc, src->tc, sizeof(env->tc));
+	memcpy(env->lc, src->lc, sizeof(env->lc));
+	memcpy(env->hc, src->hc, sizeof(env->hc));
+	memcpy(env->mc, src->mc, sizeof(env->mc));
 }
 
 struct fmt *env_fmt(void)

roff.h
@@ -364,6 +364,7 @@ void tr_divvs(char **args);
 void tr_dt(char **args);
 void tr_em(char **args);
 void tr_ev(char **args);
+void tr_evc(char **args);
 void tr_fc(char **args);
 void tr_fi(char **args);
 void tr_fp(char **args);

tr.c
@@ -1138,6 +1138,7 @@ static struct cmd {
 	{"eo", tr_eo},
 	{"eos", tr_eos},
 	{"ev", tr_ev},
+	{"evc", tr_evc},
 	{"ex", tr_ex},
 	{"fc", tr_fc},
 	{"ff", tr_ff},

commit 1532202597bce1990b49d3a4b7c21aa844f9498f

evc: clear formatting buffers
Partially filled lines should be discarded when copying environment.

Fixes: e57ccd9904b6a9b40087d8c57e9972c9f7a822b8

reg.c
@@ -324,9 +324,10 @@ void tr_evc(char **args)
 	int id = -1;
 	if (args[1])
 		id = map(args[1]);
-	if (id < 0 || !envs[id] || id == env_id)
+	if (id < 0 || !envs[id])
 		return;
 	src = envs[id];
+	env = env_alloc();
 	memcpy(env->eregs, src->eregs, sizeof(env->eregs));
 	memcpy(env->tabs, src->tabs, sizeof(env->tabs));
 	memcpy(env->tabs_type, src->tabs_type, sizeof(env->tabs_type));
@@ -334,6 +335,8 @@ void tr_evc(char **args)
 	memcpy(env->lc, src->lc, sizeof(env->lc));
 	memcpy(env->hc, src->hc, sizeof(env->hc));
 	memcpy(env->mc, src->mc, sizeof(env->mc));
+	env_free(envs[env_id]); /* free old env */
+	envs[env_id] = env;
 }
 
 struct fmt *env_fmt(void)