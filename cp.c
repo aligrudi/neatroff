aligrudi/neatroff

commit 6bda79e426e6b13c3227bcef97ef0dfa3d0131e0

cp: add \# escape sequence
\# is like \", but consumes the ending newline so that one can comment
complex input like this:
  \D't 0'\#                         set minimal drawing thickness
  \Z"\D'l 0 -\\n[nl]u-1v'"\#        left margin intersecting top margin
  \Z"\D'l 0 \\n[.t]u'\#             left margin
  \&\Z`\D'l 0 \\n[.p]u-\\n[nl]u'`\# left margin intersecting bottom margin
  \&\Z`\D'l -\\n[.o]u 0'`\#         bottom margin intersecting left margin
  \&\D'l \\n[.l]u+\\n[.o]u 0'"\#    bottom margin & intersection w/ right margin
  \Z"\D'l -\\n[.o]u 0'"\#           top margin intersecting left margin
  \Z"\D'l \\n[.l]u 0'\#             top margin
  \&\Z`\D'l \\n[.o] 0'`\#           top margin intersecting right margin
  \&\Z`\D'l 0 -\\n[nl]u-1v'`\#      right margin intersecting top margin
  \&\D'l 0 \\n[.p]u-\\n[nl]u'"\#    right margin & intersection w/ bottom margin
  \D't -1'\"                        restore regular drawing thickness

(Escape borrowed from groff.)

cp.c
@@ -245,6 +245,11 @@ int cp_next(void)
 		if (c == '"') {
 			while (c >= 0 && c != '\n')
 				c = cp_raw();
+		} else if (c == '#') {
+			while (c >= 0 && c != '\n')
+				c = cp_raw();
+			if (c >= 0)
+				c = cp_raw();
 		} else if (c == 'w' && !cp_cpmode) {
 			cp_width();
 			c = cp_next();

commit 3013d162a49babe62417016bf6075a65762e103b

cp: interpret first character of a line following \# correctly
Fixes: 6bda79e426e6b13c3227bcef97ef0dfa3d0131e0

cp.c
@@ -249,7 +249,7 @@ int cp_next(void)
 			while (c >= 0 && c != '\n')
 				c = cp_raw();
 			if (c >= 0)
-				c = cp_raw();
+				c = cp_next();
 		} else if (c == 'w' && !cp_cpmode) {
 			cp_width();
 			c = cp_next();