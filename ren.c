aligrudi/neatroff

commit 2172c4b1a72bf8a7870eb263bf5f5c35ba50d38d

ren: add .sv and .os

ren.c
@@ -171,7 +171,7 @@ static void trap_exec(int reg)
 static int ren_traps(int beg, int end, int dosp)
 {
 	int pos = trap_pos(beg);
-	if (pos >= 0 && pos < n_p && pos <= end) {
+	if (pos >= 0 && (cdiv || pos < n_p) && pos <= end) {
 		if (dosp && pos > beg)
 			ren_sp(pos - beg);
 		trap_exec(trap_reg(beg));
@@ -259,6 +259,23 @@ void tr_sp(char **args)
 	if (args[0][0] == '.')
 		ren_br(1);
 	down(args[1] ? eval(args[1], 0, 'v') : n_v);
+}
+
+void tr_sv(char **args)
+{
+	int n = eval(args[1], 0, 'v');
+	n_sv = 0;
+	if (n_d + n < f_nexttrap())
+		down(n);
+	else
+		n_sv = n;
+}
+
+void tr_os(char **args)
+{
+	if (n_sv)
+		down(n_sv);
+	n_sv = 0;
 }
 
 void tr_mk(char **args)

tr.c
@@ -474,6 +474,7 @@ static struct cmd {
 	{"ne", tr_ne},
 	{"nf", tr_nf},
 	{"nr", tr_nr, mkargs_reg1},
+	{"os", tr_os},
 	{"pl", tr_pl},
 	{"pn", tr_pn},
 	{"po", tr_po},
@@ -484,6 +485,7 @@ static struct cmd {
 	{"rt", tr_rt},
 	{"so", tr_so},
 	{"sp", tr_sp},
+	{"sv", tr_sv},
 	{"ti", tr_ti},
 	{"tm", tr_tm, mkargs_eol},
 	{"vs", tr_vs},

xroff.h
@@ -131,10 +131,12 @@ void tr_ll(char **args);
 void tr_mk(char **args);
 void tr_ne(char **args);
 void tr_nf(char **args);
+void tr_os(char **args);
 void tr_pn(char **args);
 void tr_ps(char **args);
 void tr_rt(char **args);
 void tr_sp(char **args);
+void tr_sv(char **args);
 void tr_ti(char **args);
 void tr_wh(char **args);
 
@@ -212,6 +214,7 @@ void adj_conf(struct adj *adj, int *ll, int *in, int *ti);
 #define n_na		(*nreg(REG(0, 'n')))	/* .na mode */
 #define n_o0		(*nreg(REG(0, 'o')))	/* last .o */
 #define n_s0		(*nreg(REG(0, 's')))	/* last .s */
+#define n_sv		(*nreg(REG(0, 'S')))	/* .sv value */
 #define n_v0		(*nreg(REG(0, 'v')))	/* last .v */
 
 /* functions for implementing read-only registers */

commit bc5adbf4650fcf46f6b91d1fe7d6e2ff5780987a

ren: fix sv
Register .t contains distance to next trap, not its position.

Also, <= needs to be used to get the same behavior as Plan 9 and
Heirloom troff. (Geometry of ne and sv differs.)

Fixes: 2172c4b1a72bf8a7870eb263bf5f5c35ba50d38d

ren.c
@@ -447,7 +447,7 @@ void tr_sv(char **args)
 {
 	int n = args[1] ? eval(args[1], 'v') : n_v;
 	n_sv = 0;
-	if (n_d + n < f_nexttrap())
+	if (n <= f_nexttrap())
 		down(n);
 	else
 		n_sv = n;